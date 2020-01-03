96c2f11fad2adc86e4be9228f5bc032db90c89e6
ken <123123@qq.com>
2020-01-03 14:00:03 +0800

    限流的实现

--- shop_gateway/src/main/java/com/qf/shop_gateway/ShopGatewayApplication.java
+++ shop_gateway/src/main/java/com/qf/shop_gateway/ShopGatewayApplication.java
@@ -2,7 +2,11 @@ package com.qf.shop_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication(scanBasePackages = "com.qf")
 @EnableEurekaClient
@@ -12,4 +16,17 @@ public class ShopGatewayApplication {
         SpringApplication.run(ShopGatewayApplication.class, args);
     }
 
+
+    @Bean(name = "remoteAddrKeyResolver")
+    public KeyResolver getKeyResolver(){
+        return new KeyResolver() {
+            @Override
+            public Mono<String> resolve(ServerWebExchange exchange) {
+//                //ip限流
+//                return Mono.just(exchange.getRequest().getRemoteAddress().getHostName());
+                //url限流
+                return Mono.just(exchange.getRequest().getURI().getPath());
+            }
+        };
+    }
 }
